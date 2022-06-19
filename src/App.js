8ba20929932e7cbc692244efacc2729283d55b28
Bhavna214 <bhavnamulchandani214@gmail.com>
2022-06-19 21:47:47 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
 
 function App() {
+  const moneyPyramid = [
+    {id:1, amount:"$ 100"},
+    {id:2, amount:"$ 200"},
+    {id:3, amount:"$ 300"},
+    {id:4, amount:"$ 500"},
+    {id:5, amount:"$ 1000"},
+    {id:6, amount:"$ 2000"},
+    {id:7, amount:"$ 4000"},
+    {id:8, amount:"$ 8000"},
+    {id:9, amount:"$ 16000"},
+    {id:10, amount:"$ 32000"},
+    {id:11, amount:"$ 64000"},
+    {id:12, amount:"$ 125000"},
+    {id:13, amount:"$ 250000"},
+    {id:14, amount:"$ 500000"},
+    {id:15, amount:"$ 1000000"},
+  ].reverse()
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="main">
+      </div>
+      <div className="pyramid">
+        <ul className="moneylist">
+          {moneyPyramid.map((m)=>(
+            <li className="money-list-item ">
+              <span className="money-list-item-number">{m.id}</span>
+              <span className="money-list-item-content">{m.amount}</span>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
