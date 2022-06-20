1b9408661663a833afc4f505a283a8c6f82db80b
Bhavna214 <80099365+Bhavna214@users.noreply.github.com>
2022-06-20 14:43:16 +0530

    Create Trivia.js

---
+++ src/components/Trivia.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function Trivia() {
+  return (
+    <div className='Trivia'>
+        <div className="question">What's my name?</div>
+        <div className="answers">
+            <div className="answer wrong">Bhavna</div>
+            <div className="answer">Bhavna</div>
+            <div className="answer">Bhavna</div>
+            <div className="answer">Bhavna</div>
+        </div>
+    </div>
+  )
+}
